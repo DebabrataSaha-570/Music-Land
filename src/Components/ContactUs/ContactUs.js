2066eb709c19cecc2eeb01208ec6a6214776af0a
Debabrata Saha <sahadebabrata570@gmail.com>
2021-10-25 08:01:31 +0600

    update

--- src/Components/ContactUs/ContactUs.js
+++ src/Components/ContactUs/ContactUs.js
@@ -1,10 +1,64 @@
 import React from 'react';
-
+import './ContactUs.css'
 const ContactUs = () => {
     return (
-        <div>
-            <h3>This is contact us component</h3>
-        </div>
+        <>
+            <section>
+
+                <div className="container my-5">
+                    <div className="row">
+                        <div className="col-md-6">
+                            <h3>Opening Hours </h3>
+                            <p className="my-3">
+                                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit vel impedit quo, blanditiis cumque quam modi alias debitis voluptatem ratione.
+                            </p>
+                            <div className="my-5">
+                                <p>
+                                    <i class="fas fa-map-marker-alt ml-icons-contact" ></i>
+                                    221B Bekar Rasta, Noakhali, Bangladesh
+                                </p>
+                                <p>
+                                    <i class="fas fa-map-marker-alt ml-icons-contact" ></i>
+                                    221B Bekar Rasta, Noakhali, Bangladesh
+                                </p>
+                                <p>
+                                    <i class="fas fa-map-marker-alt ml-icons-contact" ></i>
+                                    221B Bekar Rasta, Noakhali, Bangladesh
+                                </p>
+                            </div>
+                            <div className="ml-contact-social">
+                                <h4>Social: </h4>
+                                <i class="fab fa-facebook  fa-2x ml-social-facebook" ></i>
+                                <i class="fab fa-twitter fa-2x ml-social-twitter"></i>
+                                <i class="fab fa-youtube fa-2x ml-social-youtube"></i>
+                                <i class="fab fa-instagram-square fa-2x ml-social-instagram"></i>
+                            </div>
+                        </div>
+                        <div className="col-md-6">
+                            <form action="" className="ml-from p-5">
+                                <div className="mt-3">
+                                    <small>Your Name*:</small>
+                                    <input type="text" className="form-control" />
+                                </div>
+                                <div className="mt-3">
+                                    <small>Your Email*:</small>
+                                    <input type="text" className="form-control" />
+                                </div>
+                                <div className="mt-3">
+                                    <small>Your Message*:</small>
+                                    <textarea class="form-control" rows="3"></textarea>
+                                </div>
+                                <div className="text-center mt-4 ">
+                                    <button className="music-btn-one">Submit</button>
+                                </div>
+                            </form>
+                        </div>
+
+                    </div>
+                </div>
+            </section>
+
+        </>
     );
 };
 
--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -12,8 +12,8 @@ const Footer = () => {
                 <section className='container'>
                     <div className="row">
                         <div className="col-md-3">
-                            <h3 className="mt-5 mb-3">Music-Land</h3>
-                            <small>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur quae ipsum laborum, obcaecati dolore illum nemo iste repellendus ut nihil?</small>
+                            <h3 className="mt-5 mb-3 ml-heading">Music-Land</h3>
+                            <small className="ml-small">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur quae ipsum laborum, obcaecati dolore illum nemo iste repellendus ut nihil?</small>
                             {/* <FontAwesomeIcon icon={faCoffee} size="lg" color="blue" />
                         <FontAwesomeIcon icon={faFacebook} size="lg" color="blue" /> */}
                             <div className="ml-icons-social my-3">
@@ -24,24 +24,24 @@ const Footer = () => {
                             </div>
                         </div>
                         <div className="col-md-4">
-                            <h3 className="mt-5 mb-3 ">Contact Us</h3>
+                            <h3 className="mt-5 mb-3 ml-heading">Contact Us</h3>
                             <p>
                                 <i class="fas fa-map-marker-alt ml-icons-contact" ></i>
-                                <small className="mx-2">221B Bekar Rasta, Noakhali, Bangladesh</small>
+                                <small className="mx-2 ml-small">221B Bekar Rasta, Noakhali, Bangladesh</small>
                             </p>
                             <p>
                                 <i class="fas fa-phone-alt ml-icons-contact"></i>
-                                <small className="mx-2">0173829834984</small>
+                                <small className="mx-2 ml-small">0173829834984</small>
                             </p>
                             <p>
                                 <i class="fas fa-envelope ml-icons-contact"></i>
-                                <small className="mx-2">admin@gmail.com</small>
+                                <small className="mx-2 ml-small">admin@gmail.com</small>
                             </p>
 
                         </div>
                         <div className="col-md-5">
-                            <h3 className="mt-5 mb-3">Instagram</h3>
-                            <small>Instagram did not return a 200.</small>
+                            <h3 className="mt-5 mb-3 ml-heading">Instagram</h3>
+                            <small className='ml-small'>Instagram did not return a 200.</small>
                         </div>
                     </div>
 
