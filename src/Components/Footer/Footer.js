3bab9c1b5b4316555b7c609517c8d4c799bcb208
Debabrata Saha <sahadebabrata570@gmail.com>
2021-10-25 06:55:03 +0600

    update

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,10 +1,54 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee, faFacebook, faTwitter } from '@fortawesome/free-solid-svg-icons'
+import './Footer.css'
 
 const Footer = () => {
     return (
-        <div>
-            <h3>this is footer component</h3>
-        </div>
+        <>
+            <footer className="d-flex align-items-center" >
+
+
+                <section className='container'>
+                    <div className="row">
+                        <div className="col-md-3">
+                            <h3 className="mt-5 mb-3">Music-Land</h3>
+                            <small>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur quae ipsum laborum, obcaecati dolore illum nemo iste repellendus ut nihil?</small>
+                            {/* <FontAwesomeIcon icon={faCoffee} size="lg" color="blue" />
+                        <FontAwesomeIcon icon={faFacebook} size="lg" color="blue" /> */}
+                            <div className="ml-icons-social my-3">
+                                <i class="fab fa-facebook  fa-2x" ></i>
+                                <i class="fab fa-twitter fa-2x"></i>
+                                <i class="fab fa-youtube fa-2x"></i>
+                                <i class="fab fa-instagram-square fa-2x"></i>
+                            </div>
+                        </div>
+                        <div className="col-md-4">
+                            <h3 className="mt-5 mb-3 ">Contact Us</h3>
+                            <p>
+                                <i class="fas fa-map-marker-alt ml-icons-contact" ></i>
+                                <small className="mx-2">221B Bekar Rasta, Noakhali, Bangladesh</small>
+                            </p>
+                            <p>
+                                <i class="fas fa-phone-alt ml-icons-contact"></i>
+                                <small className="mx-2">0173829834984</small>
+                            </p>
+                            <p>
+                                <i class="fas fa-envelope ml-icons-contact"></i>
+                                <small className="mx-2">admin@gmail.com</small>
+                            </p>
+
+                        </div>
+                        <div className="col-md-5">
+                            <h3 className="mt-5 mb-3">Instagram</h3>
+                            <small>Instagram did not return a 200.</small>
+                        </div>
+                    </div>
+
+                </section>
+            </footer>
+
+        </>
     );
 };
 
